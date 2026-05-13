f27eec7f3fa86f9707c5764141f02b51e17f91c4
Milk-Cool <coder.tomsk@gmail.com>
2026-05-13 22:35:33 +0300

    fix slow voice rendering on cardputer

--- patchlib.py
+++ patchlib.py
@@ -2,24 +2,25 @@ Import("env")
 
 from os.path import join, exists
 
-PATH_A = join(".pio", "libdeps", "esp32dev", "BackgroundAudio", "src", "BackgroundAudioSpeech.h")
-PATH_B = join(".pio", "libdeps", "esp32dev", "BackgroundAudio", "src", "libespeak-ng", "synthesize.h")
+for target in "esp32dev", "cardputer":
+    PATH_A = join(".pio", "libdeps", target, "BackgroundAudio", "src", "BackgroundAudioSpeech.h")
+    PATH_B = join(".pio", "libdeps", target, "BackgroundAudio", "src", "libespeak-ng", "synthesize.h")
 
-def patch():
-    if not exists(PATH_A) or not exists(PATH_B):
-        return
-    f = open(PATH_A, "r")
-    d = f.read()
-    f.close()
-    f = open(PATH_A, "w")
-    f.write(d.replace(" _out->setBuffers(5, framelen);", " _out->setBuffers(1, framelen);"))
-    f.close()
+    def patch():
+        if not exists(PATH_A) or not exists(PATH_B):
+            return
+        f = open(PATH_A, "r")
+        d = f.read()
+        f.close()
+        f = open(PATH_A, "w")
+        f.write(d.replace(" _out->setBuffers(5, framelen);", " _out->setBuffers(1, framelen);"))
+        f.close()
 
-    f = open(PATH_B, "r")
-    d = f.read()
-    f.close()
-    f = open(PATH_B, "w")
-    f.write(d.replace("#define N_PHONEME_LIST 1000", "#define N_PHONEME_LIST 250"))
-    f.close()
+        f = open(PATH_B, "r")
+        d = f.read()
+        f.close()
+        f = open(PATH_B, "w")
+        f.write(d.replace("#define N_PHONEME_LIST 1000", "#define N_PHONEME_LIST 250"))
+        f.close()
 
-patch()
+    patch()
